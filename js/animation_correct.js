56449fe2aa01d360d892763053e16ae8fbe2bd8b
sarwesh <sarwesh.krshnn@gmail.com>
2014-04-14 18:32:12 +0530

    animation corrected

--- js/animation_correct.js
+++ js/animation_correct.js
@@ -36,7 +36,7 @@ function animation_one(){
 		defsty('#list5',h/2,w/2,w/15,w/15,1);
 		move('#list1',w/2-sc_r,h/4-sc_r,1);
 		move('#list5',w/2-sc_r,h/4-sc_r,1);
-	},4);
+	},500);
 	
 	animation_two();
 }
@@ -47,17 +47,17 @@ function animation_two(){
 		defsty('#list6',h/4-sc_r,w/2-sc_r,w/15,w/15,1);
 		move('#list3',5*w/8-sc_r,5*h/8-sc_r,1);
 		move('#list6',5*w/8-sc_r,5*h/8-sc_r,1);
-	},500);
+	},1000);
 	animation_three();
 }
 
 function animation_three(){
 	setTimeout(function(){
-		defsty('#list4',4.9*h/8-sc_r/2,5*w/8-sc_r/2,w/15,w/15,1);
-		defsty('#list7',4.9*h/8-sc_r/2,5*w/8-sc_r/2,w/15,w/15,1);
+		defsty('#list4',5*h/8-sc_r/2,5*w/8-sc_r/2,w/15,w/15,1);
+		defsty('#list7',5*h/8-sc_r/2,5*w/8-sc_r/2,w/15,w/15,1);
 		move('#list4',3.1*w/8-sc_r,5*h/8-sc_r,1);
 		move('#list7',3.1*w/8-sc_r,5*h/8-sc_r,1);
-	},1000);
+	},1500);
 }
 
 function animation_four(){
@@ -69,7 +69,6 @@ function animation_four(){
 		move('#list5',0.2*w/15,h/2-w/30,2);
 		move('#list6',0.2*w/15,h/2-w/12-w/30,3);
 		move('#list7',0.2*w/15,h/2+w/12-w/30,4);
-
 	},4);
 	setTimeout(function(){
 	},4);
